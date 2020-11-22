533f52a0f0eb864b5460201c95e76be443e795e6
Azharul Islam <32319631+azhardotme@users.noreply.github.com>
2020-11-23 01:50:58 +0600

    Added some program

--- index.php
+++ index.php
@@ -1,10 +1,44 @@
 <?php
 echo "Hello PHP!";
 echo"<br/>";
+
+// Data Types
+//String
+$txt = "Hello Nabila!";
+echo($txt);
+echo"<br/>";
+
+// Integer
 $x = 6;
-$txt = "Hello Azhar!";
 echo ($x);
 echo"<br/>";
-echo($txt);
+var_dump($x);
+echo"<br/>";
+// Float
+$y = 12.22;
+var_dump($y);
+echo"<br/>";
+
+//Array
+
+$cars = array("Volvo","BMW","Toyota","Corolla");
+var_dump($cars);
+echo"<br/>";
+
+// PHP String
+echo strlen("Mymensingh");
+echo"<br/>";
+echo str_word_count("Hello Azhar");
+echo"<br/>";
+
+echo strrev("Hello Azhar");
+echo"<br/>";
+
+echo(max(5,8,7,1,4));
+echo"<br/>";
+echo(min(45,8,5,44,0));
+echo"<br/>";
+
+
 
 ?>
